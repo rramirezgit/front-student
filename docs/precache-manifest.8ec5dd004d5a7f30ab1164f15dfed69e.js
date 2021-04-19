daacf3c32102bfc55faa8a701b5e23deb508b921
unknown <97.rramirez@gmail.com>
2021-04-19 17:04:50 -0300

    docs

---
+++ docs/precache-manifest.8ec5dd004d5a7f30ab1164f15dfed69e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fca9ade5cd380a982174",
+    "url": "/static/css/main.f1c79562.chunk.css"
+  },
+  {
+    "revision": "fca9ade5cd380a982174",
+    "url": "/static/js/main.fca9ade5.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7906f892cca05a1c2a3f",
+    "url": "/static/css/2.593d6765.chunk.css"
+  },
+  {
+    "revision": "7906f892cca05a1c2a3f",
+    "url": "/static/js/2.7906f892.chunk.js"
+  },
+  {
+    "revision": "88ab078724f31aa906654b72c0fc5175",
+    "url": "/static/media/studenst.88ab0787.svg"
+  },
+  {
+    "revision": "b903cc90444916b625d4cf03bdf4f8f3",
+    "url": "/static/media/nerd.b903cc90.svg"
+  },
+  {
+    "revision": "543471db9334e13f33ace9384d5568ef",
+    "url": "/index.html"
+  }
+];
